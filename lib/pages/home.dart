9ada221747a949251b5b7bcd885c9201b9dfe9fc
yahyeabdinasir <yahyeabdinasirfarah@gmail.com>
2026-04-03 11:01:09 +0300

    feat : added the alert dialog and text field

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -17,27 +18,71 @@ class MyDesing extends StatefulWidget {
 }
 
 class _MyDesingState extends State<MyDesing> {
+  final WaterController = TextEditingController();
+
+  void ShowDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Add Water"),
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("add water to your daily intake"),
+            SizedBox(height: 20,),
+            TextField(
+              textAlign: TextAlign.center,
+              controller: WaterController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp("[0-9]")),
+              ],
+
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                // floatingLabelBehavior: FloatingLabelBehavior.never,
+                // alignLabelWithHint: true,
+                labelText: "Amount",
+              ),
+            ),
+          ],
+        ),
+        // this action it's list of the widget it's the attribute that belongs the alert dialog
+
+        actions: [
+
+          TextButton(onPressed: (){
+            Navigator.pop(context);
+
+          }, child: Text("cancel")),
+          TextButton(onPressed: (){
+
+          }, child: Text("save")),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Water Take"),
         actions: [
-          IconButton(onPressed: () {
-            print("hello map ");
-    },
-      icon: Icon(Icons.map),
-
-          )
-
-
+          IconButton(
+            onPressed: () {
+              print("hello map ");
+            },
+            icon: Icon(Icons.map),
+          ),
         ],
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print("hello yahye ");
+          ShowDialog();
         },
         child: Icon(Icons.add),
       ),
